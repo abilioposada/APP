5626196b64058ea57e6fcc16579ce4bbf65bb98d
Abilio Posada <abilioposada@gmail.com>
2022-03-31 03:23:59 -0600

    Add link to authors from the main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,8 @@
                     </svg>
                 </div>
 
+                <a style="color: gray" href="{{ url( 'authors' ) }}">CLICK TO AUTHORS</a>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
